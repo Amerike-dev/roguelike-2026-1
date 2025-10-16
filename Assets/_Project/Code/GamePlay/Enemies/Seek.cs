25ed9f9d7512b9b359bdee697ef55198f2b2c8ae
Angel <chiv_157@hotmail.com>
2025-10-15 18:43:05 -0600

    PR-16: Seek-add

---
+++ Assets/_Project/Code/GamePlay/Enemies/Seek.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Seek
+{
+    private Transform _target;
+    private float _speed;
+    private float _velocity;
+    private Transform _enemy;
+    private float steering;
+    
+    public Seek()
+    {
+        
+    }
+    public void GetSteering()
+    {
+        //_velocity = (_target.position);
+        //steering=()
+    }
+}
