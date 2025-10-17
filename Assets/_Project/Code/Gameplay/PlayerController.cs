18ceecb6259f8e8e9c9a714b9117bfd2b0d17151
Jorge Salas Leybon <146293962+JorSL1120@users.noreply.github.com>
2025-10-17 13:57:24 -0600

    modificando algo del PlayerController

--- Assets/_Project/Code/Gameplay/PlayerController.cs
+++ Assets/_Project/Code/Gameplay/PlayerController.cs
@@ -1,29 +1,41 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
 {
-    public Player player;
+    private Player player;
     private Rigidbody2D rb;
-    private SpriteRenderer spriteRenderer;
+    private GameObject playerGO;
     [SerializeField] private Sprite capsuleSprite;
     
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        player = new Player(rb);
-        rb.gravityScale = 0f;
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        playerGO = new GameObject("Player");
+        playerGO.transform.parent = transform;
         
+        SpriteRenderer spriteRenderer = playerGO.AddComponent<SpriteRenderer>();
         if(capsuleSprite != null) spriteRenderer.sprite = capsuleSprite;
+
+        rb = playerGO.AddComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        playerGO.AddComponent<CapsuleCollider2D>();
+
+        player = new Player(rb);
     }
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
         player.PlayerMovement(horizontalInput, verticalInput);
+        
+        SystemCombat();
+    }
+
+    public void SystemCombat()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Atacando ando");
+        }
     }
 }
