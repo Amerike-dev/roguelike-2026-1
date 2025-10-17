5cbc4e7f5f8796a167eb202e5c1f2d46c98d27d0
Jorge Salas Leybon <146293962+JorSL1120@users.noreply.github.com>
2025-10-17 13:38:26 -0600

    ya se mueve el player y tambien se le pone una capsula de sprite

--- Assets/_Project/Code/Gameplay/PlayerController.cs
+++ Assets/_Project/Code/Gameplay/PlayerController.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
 {
     public Player player;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite capsuleSprite;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         player = new Player(rb);
         rb.gravityScale = 0f;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        if(capsuleSprite != null) spriteRenderer.sprite = capsuleSprite;
     }
 
     void Update()
