4dc5f76d9097705223629b6f8212faaa42e5ddde
Ophion-73 <cdmx2754@amerike.edu.mx>
2025-10-25 13:41:02 -0600

    ya acabe
    pequeno problema, la ruta del archivo depende de la computadora de cada quien. guardar los archivos en Application.persistentDataPath seria lo mejor para que no haya problemas

--- Assets/_Project/Code/Core/DBController.cs
+++ Assets/_Project/Code/Core/DBController.cs
@@ -1,15 +1,37 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 using UnityEngine.Windows;
 
 public class DBController : MonoBehaviour
 {
     public string jsonLocation; 
     public DB db;
-
+    public TMP_InputField playerName;
+    public TextMeshProUGUI nameDisplay;
+    public Button saveButton;
+    
     private void Awake()
     {
         db = new DB(jsonLocation);
     }
 
+    private void Start()
+    {
+        playerName.onValueChanged.AddListener(ChangeName);
+        nameDisplay.text = "name: " + db.Read(playerData.name);
+    }
+
+    public void ChangeName(string name)
+    {
+        db.Write(playerData.name, name);
+        nameDisplay.text = "name: " + db.Read(playerData.name);
+    }
+
+    public void SaveName()
+    {
+        db.Save();
+    }
 
 }
