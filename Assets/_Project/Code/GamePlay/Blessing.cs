d413c67d8c23126b99c14054fe474097aecda3cc
Jorge Salas Leybon <146293962+JorSL1120@users.noreply.github.com>
2025-11-23 17:39:07 -0600

    :(

---
+++ Assets/_Project/Code/GamePlay/Blessing.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+
+public enum TypeBlessing {  }
+public class Blessing : MonoBehaviour
+{
+    public TypeBlessing typeBlessing;
+    public string name;
+}
---
+++ Assets/_Project/Code/GamePlay/BlessingController.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class BlessingController : MonoBehaviour
+{
+    
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        throw new NotImplementedException();
+    }
+}
